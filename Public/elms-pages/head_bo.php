b6892a1abe12a512a7fe5ab9d00631037545fdb2
hummel <hummel.julien.pro@gmail.com>
2021-11-10 20:21:49 +0100

    Fixe title

--- Public/elms-pages/head_bo.php
+++ Public/elms-pages/head_bo.php
@@ -7,6 +7,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/dashboard-main.css">
     <script src="https://cdn.jsdelivr.net/npm/chart.js@3.5.1/dist/chart.min.js"></script>
-    <title>Document</title>
+    <title>BackOffice SuperMontre</title>
 
 </head>
