0895001c3043822fe08dc1b67e262881b8206cf0
hummel <hummel.julien.pro@gmail.com>
2022-02-04 12:01:43 +0100

    commit befor majeur version

--- vue/supermontre/src/main.js
+++ vue/supermontre/src/main.js
@@ -1,5 +1,44 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import { createStore } from 'vuex';
+import RootComponent from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+const store = createStore({
+    state() {
+        let products
+        return {
+            products,
+        }
+    },
+    mutations: {
+        productsRecever(state, products) {
+            state.products = products
+        }
+    },
+    getters: {
+        products(state) {
+            return state.products
+        }
+    }
+})
+
+const App = createApp(RootComponent)
+App.use(router)
+App.use(store)
+App.mount('#app')
+
+
+function getDataProducts() {
+    return fetch("http://localhost/api/products", {
+        method: "GET",
+    })
+        .then(function (response) {
+            console.log("reponse");
+            return response.json();
+        })
+        .catch(function (response) {
+            console.error(response);
+        });
+}
+
+getDataProducts().then((data) => store.commit("productsRecever", data));
